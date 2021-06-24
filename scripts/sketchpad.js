b4c52f1fa5eec89866f1a7c470096da01867cb2e
Lukas Erekson <50251026+LukasErekson@users.noreply.github.com>
2021-06-24 10:21:07 -0600

    Fix bug in drawSketchpad function
    - Make it so that calling drawSketchpad with a different row length results in
      removing or adding squares, adjusts the size, and resets the background color
      of each of the squares.

--- scripts/sketchpad.js
+++ scripts/sketchpad.js
@@ -1,6 +1,7 @@
 'using strict';
 
 // Create the sketch pad squares
+const SKETCHPADWIDTH = 600;
 const sketchpad = document.getElementById('sketchpad-wrapper');
 drawSketchpad();
 
@@ -10,34 +11,36 @@ drawSketchpad();
  * @param {Number} squareSize The size (in pixels) of each square.
  * @param {Number} rowLength  The number of squares in each row/column.
  */
-function drawSketchpad(rowLength=4){
+function drawSketchpad(rowLength=16){
 
-    let squareSize = Math.round(600 / rowLength);
+    let squareSize = SKETCHPADWIDTH / rowLength;
 
-    // Adjust size so flexbox works as intended.
-    // sketchpad.style.width = `${squareSize*(rowLength)}px`;
-    // sketchpad.style.height = `${squareSize*(rowLength)}px`;
 
-    // Used in several places so store asa a variable.
+    // Used in several places so store as a a variable.
     let numChildren = sketchpad.children.length;
+  
     let numNewSquares = rowLength**2 - numChildren;
 
     while (numNewSquares < 0) {
         sketchpad.removeChild(sketchpad.children[numChildren - 1]);
         numNewSquares++;
+        numChildren--;
     }
 
-    if (numNewSquares > 0) {
-        for (let i = numChildren; i < rowLength**2; i++) {
-            let padSquare = document.createElement('div');
-            padSquare.classList.add('sketchpad-square');
-            padSquare.setAttribute('id', `${i}`);
-            padSquare.setAttribute('style', `width:${squareSize}px; height:${squareSize}px;`);
-            padSquare.style.backgroundColor = 'rgb(204, 204, 204)';
-            padSquare.addEventListener('mouseover', drawOnSketchpad);
-            
-            sketchpad.appendChild(padSquare);
-        }
+    for (let i = 0; i < rowLength**2; i++) {
+      let padSquare = document.getElementById(i);
+      if (padSquare === null) {
+        padSquare = document.createElement('div');
+        padSquare.classList.add('sketchpad-square');
+        padSquare.setAttribute('id', `${i}`);
+        sketchpad.appendChild(padSquare);
+      }
+
+      padSquare.setAttribute('style', `width:${squareSize}px; height:${squareSize}px;`);
+      padSquare.style.backgroundColor = 'rgb(204, 204, 204)';
+      padSquare.addEventListener('mouseover', drawOnSketchpad);
+
+      
     }
 }
 
@@ -54,4 +57,3 @@ function drawOnSketchpad(event) {
 
     targetedSquare.style.backgroundColor = `rgb(${currentColor[0] - 20}, ${currentColor[1] - 20}, ${currentColor[2] - 20})`;
 }
-
